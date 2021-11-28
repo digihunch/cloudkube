92edcb49f3d820ca840d799b1de8b0cc46f24cf1
Yi Lu <y_lu@live.com>
2021-11-28 18:10:17 -0500

    remove reference to node resource group from rbac, as it requires read permission

--- azure/modules/rbac/data.tf
+++ azure/modules/rbac/data.tf
@@ -1,6 +1,6 @@
 data "azurerm_resource_group" "default" {
   name = var.resource_group
 }
-data "azurerm_resource_group" "node_resource_group" {
-  name = var.rbac_aks_node_resource_group
-}
+#data "azurerm_resource_group" "node_resource_group" {
+#  name = var.rbac_aks_node_resource_group
+#}
