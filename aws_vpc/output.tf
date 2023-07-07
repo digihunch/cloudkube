62754f20128577d2a8fddf8a3936f28c5a548162
Yi Lu <ylu@digihunch.com>
2023-07-06 20:09:07 -0400

    aws_vpc update

--- aws_vpc/output.tf
+++ aws_vpc/output.tf
@@ -1,3 +1,16 @@
 output "bastion" {
   value = join(",",data.aws_instances.bastion.ids)
 }
+output "vpc_id" {
+  value = aws_vpc.eks_vpc.id 
+}
+output "node_subnets_ids" {
+  value = join(",",aws_subnet.nodesubnets.*.id)
+}
+output "node_subnets_azs" {
+  value = join(",",aws_subnet.nodesubnets.*.availability_zone)
+}
+
+output "bastion_sg_id" {
+  value = aws_security_group.bastionsecgrp.id
+}
